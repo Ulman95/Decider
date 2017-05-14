a6a91d503b7d5983c6661daea9dec9b8d31d4d9b
Vyacheslav Rodionov <rodionovSlava95@gmail.com>
2017-05-14 18:50:56 +0300

    added fonts added phrases

--- app/src/main/java/com/ulman/decider/mvp/model/font/AndroidFontContainer.kt
+++ app/src/main/java/com/ulman/decider/mvp/model/font/AndroidFontContainer.kt
@@ -8,14 +8,26 @@ import com.ulman.decider.mvp.model.font.base.FontContainer
 class AndroidFontContainer(val context: Context) : FontContainer {
 
     val fonts = listOf(
-            PoiretOneFont(context.assets),
-            ComfortaaFont(context.assets),
-            JuraFont(context.assets),
-            NeuchaFont(context.assets),
-            UnderdogFont(context.assets)
+            "fonts/Neucha.ttf",
+            "fonts/Comfortaa-Regular.ttf",
+            "fonts/Jura-Regular.ttf",
+            "fonts/PoiretOne-Regular.ttf",
+            "fonts/Underdog-Regular.ttf",
+            "fonts/AC Line.otf",
+            "fonts/beer money.ttf",
+            "fonts/BERNIERDistressed-Regular.otf",
+            "fonts/Cheque-Regular.otf",
+            "fonts/gnyrwn971.ttf",
+            "fonts/GUERRILLA-Normal.otf",
+            "fonts/Lionelofparis.ttf",
+            "fonts/TkachenkoSketch4F-Regular.otf",
+            "fonts/ZnikomitNo25.otf"
     )
 
-    override fun getFont(choice: Choice): Font = fonts[choice.which]
+    override fun getFont(choice: Choice): Font = object : Font(context.assets) {
+
+        override fun getPath(): String = fonts[choice.which]
+    }
 
     override fun getFontCount(): Int = fonts.size
 
--- app/src/main/java/com/ulman/decider/mvp/model/phrase/CoinsPhrase.kt
+++ app/src/main/java/com/ulman/decider/mvp/model/phrase/CoinsPhrase.kt
@@ -2,45 +2,19 @@ package com.ulman.decider.mvp.model.phrase
 
 import android.content.Context
 import com.ulman.decider.R
-import com.ulman.decider.mvp.model.decider.beans.Choice
 import com.ulman.decider.mvp.model.decider.DeciderType
+import com.ulman.decider.mvp.model.decider.beans.Choice
 
 class CoinsPhrase(val context: Context) : Phrase {
 
-    private companion object {
-
-        val COINS_PHRASE_YES: List<Int> = listOf(
-                R.string.phrase_coins_yes_1,
-                R.string.phrase_coins_yes_2,
-                R.string.phrase_coins_yes_3,
-                R.string.phrase_coins_yes_4,
-                R.string.phrase_coins_yes_5,
-                R.string.phrase_coins_yes_6,
-                R.string.phrase_coins_yes_7
-        )
-
-        val COINS_PHRASE_NO: List<Int> = listOf(
-                R.string.phrase_coins_no_1,
-                R.string.phrase_coins_no_2,
-                R.string.phrase_coins_no_3,
-                R.string.phrase_coins_no_4,
-                R.string.phrase_coins_no_5,
-                R.string.phrase_coins_no_6,
-                R.string.phrase_coins_no_7
-        )
-
-        val COINS_SIZE = COINS_PHRASE_YES.size
-
-    }
+    val yesPhrases: Array<out String> = context.resources.getStringArray(R.array.phrase_coins_yes)!!
+    val noPhrases: Array<out String> = context.resources.getStringArray(R.array.phrase_coins_no)!!
 
     override fun getPhrase(choice: Choice, deciderType: DeciderType): String {
 
-        val text = if (deciderType == DeciderType.YES) COINS_PHRASE_YES[choice.which] else COINS_PHRASE_NO[choice.which]
-
-        return context.getString(text)
-
+        return if (deciderType == DeciderType.YES) yesPhrases[choice.which] else noPhrases[choice.which]
     }
 
-    override fun getPhraseCount(): Int = COINS_SIZE
+    override fun getPhraseCount(): Int = yesPhrases.size
 
 }
